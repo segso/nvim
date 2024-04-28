2f0e5b13d0fcc7c905cd99ab29a4b92087c1010e
SEG <segdev.mx@gmail.com>
2024-04-27 22:09:50 -0600

    feat(settings): add some vanilla settings

---
+++ lua/core.lua
@@ -0,0 +1,20 @@
+vim.opt.termguicolors = true
+
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 1
+
+vim.opt.cursorline = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+
+vim.opt.scrolloff = 7
+vim.opt.lazyredraw = true
+vim.opt.linebreak = true
